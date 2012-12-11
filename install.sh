308ced2d2c07f94f5a5b6e08f499eb5c4535045e
Esteban Pastorino <ejpastorino@gmail.com>
2012-12-10 23:38:23 -0300

    add install script

---
+++ install.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+me=`basename $0`
+dir=`dirname $0`
+
+cd $dir
+
+echo "=== Symlinking dotfiles"
+for src in $(find $PWD -maxdepth 1)
+  do
+    file=`basename $src`
+
+      if [[
+            $file != '.git'                  &&
+            $file != '.'                     &&
+            $file != '..'                    &&
+            $file != $me
+         ]]
+         then
+            echo "Symlinking .$file from $src"
+            ln -Ffs $src ~/.$file
+         fi
+  done
+# Resume previous directory
+cd - > /dev/null
+
+echo "=== Done!"
